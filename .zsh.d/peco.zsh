9290f76831d130f79d246bcf2988cdbc9f82b861
ringtail003 <ringtail003@gmail.com>
2016-11-23 22:00:35 +0900

    [zsh] C-rの結果をpecoに渡す

---
+++ .zsh.d/peco.zsh
@@ -0,0 +1,11 @@
+function peco-select-history() {
+    # historyを番号なし、逆順、最初から表示。
+    # 順番を保持して重複を削除。
+    # カーソルの左側の文字列をクエリにしてpecoを起動
+    # \nを改行に変換
+    BUFFER="$(history -nr 1 | awk '!a[$0]++' | peco --query "$LBUFFER" | sed 's/\\n/\n/')"
+    CURSOR=$#BUFFER             # カーソルを文末に移動
+    zle -R -c                   # refresh
+}
+zle -N peco-select-history
+bindkey '^R' peco-select-history
